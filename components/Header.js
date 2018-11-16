65cac4020e649a00b5f5920f1a6ae6b52510a2ba
siddhant1 <sidhant.manchanda@gmail.com>
2018-11-16 19:27:57 +0530

    Added Search

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Router from "next/router";
 import NProgress from "nprogress";
 import Cart from "./Cart";
+import Search from "./Search";
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -61,7 +62,7 @@ const Header = props => {
         <Nav />
       </div>
       <div className="sub-bar">
-        <p>Search</p>
+        <Search />
       </div>
       <Cart />
     </StyledHeader>
